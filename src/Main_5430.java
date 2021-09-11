0a192c1aa463b2929cf2127c01956ee59f8050bf
sukeun1997 <busan-sy@naver.com>
2021-09-11 23:05:55 +0900

    백준 9 / 11 수정

--- src/Main_5430.java
+++ src/Main_5430.java
@@ -12,50 +12,45 @@ public class Main_5430 {
         for (int i = 0; i < T; i++) {
             Deque<Integer> deque = new LinkedList<>();
             Deque<Integer> ndeque = new LinkedList<>();
-            boolean error = false;
-            String S = br.readLine();
-            List<String> AC = new ArrayList<>();
-            for (int z = 0; z < S.length(); z++) {
-                AC.add(String.valueOf(S.charAt(z)));
-            }
+            boolean error = false, Back = false;
+            String AC = br.readLine();
             int N = Integer.parseInt(br.readLine());
             String list_s = br.readLine();
             String[] list = list_s.substring(1,list_s.length()-1).split(",");
             for (int l = 0; l < N; l++) {
                 deque.add(Integer.parseInt(list[l]));
             }
-            while (true) {
-                if (AC.isEmpty()) {
-                    break;
-                }
-                if (AC.get(0).equals("R")) {
-                    if(AC.size() >= 2 && AC.get(0).equals("R") && AC.get(1).equals("R")) {
-                        AC.remove(0);
-                        AC.remove(0);
+            for (char cmd : AC.toCharArray()) {
+                if (cmd == 'R') {
+                    if (Back == true) {
+                        Back = false;
                     } else {
-                        ndeque.clear();
-                        Iterator x = deque.descendingIterator();
-                        while (x.hasNext()) {
-                            ndeque.add((Integer) x.next());
-                        }
-                        AC.remove(0);
-                        deque.clear();
-                        deque.addAll(ndeque);
+                        Back = true;
                     }
-                } else if (AC.get(0).equals("D")) {
+
+                } else if (cmd == 'D') {
                     if (deque.isEmpty()) {
                         stringBuilder.append("error").append("\n");
                         error = true;
                         break;
                     } else {
-                        deque.pop();
+                        if(Back)
+                            deque.removeLast();
+                        else
+                            deque.removeFirst();
                     }
-                    AC.remove(0);
                 }
             }
             if(!error) {
                 if (!deque.isEmpty()) {
-                    stringBuilder.append(deque).append("\n");
+                    StringBuilder sb = new StringBuilder("[");
+                    while (!deque.isEmpty()) {
+                        sb.append(Back ? deque.removeLast() : deque.removeFirst());
+                        if (deque.size() != 0)
+                            sb.append(',');
+                    }
+                    sb.append(']').append("\n");
+                    stringBuilder.append(sb);
                 } else {
                     stringBuilder.append("[]").append("\n");
                 }
