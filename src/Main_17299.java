c99f22681c09acb807ee3590193fd470051f2ac6
sukeun1997 <busan-sy@naver.com>
2021-09-17 17:56:00 +0900

    백준 9 / 17

--- src/Main_17299.java
+++ src/Main_17299.java
@@ -1,37 +1,25 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-/*
-list 3 5 2 7
-
-                i = 0   i = 1   i = 2   i = 3
-
-(전)stack       0          0        1       1,2
-
-ans                        1       1         1 3 3
-
-(후)stack                  1        1,2       3
-
-
-
-
- */
-public class Main_17298 {
+public class Main_17299 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder stringBuilder = new StringBuilder();
         int N = sc.nextInt();
 
+        int list_size[] = new int[1000001];
         int list[] = new int[N];
         int Ans[] = new int[N];
         Stack<Integer> stack2 = new Stack<>();
         for (int i = 0; i < N; i++) {
-            list[i] = sc.nextInt();
+            int a = sc.nextInt();
+            list[i] = a;
+            list_size[a] += 1;
         }
 
         stack2.push(0);
         for (int i = 0; i < N; i++) {
-            while (!stack2.isEmpty() && list[stack2.peek()] < list[i]) {
+            while (!stack2.isEmpty() && list_size[list[stack2.peek()]] < list_size[list[i]]) {
                 Ans[stack2.pop()] = i;
             }
             stack2.push(i);
