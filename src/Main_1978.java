4b6654fc35a661a45e9bdf996e9e89a44314338a
sukeun1997 <busan-sy@naver.com>
2021-09-18 13:35:04 +0900

    백준 9 / 18

---
+++ src/Main_1978.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main_1978 {
+    static int N[] = new int[1001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int count[] = new int[T];
+        for (int i = 0; i < T; i++) {
+            count[i] = sc.nextInt();
+        }
+        for (int i = 2; i <= 31; i++) {
+            if(N[i] == -1)
+                continue;
+            for (int l = i*i; l <= 1000; l++) {
+                if (l % i == 0 && l != i) {
+                    N[l] = -1;
+                }
+            }
+        }
+        int num = 0;
+
+        for (int x : count) {
+            if (isPrime(x)) {
+                num++;
+            }
+        }
+        System.out.println(num);
+
+
+    }
+
+    public static boolean isPrime(int x){
+        if (N[x] == 0 && x != 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
