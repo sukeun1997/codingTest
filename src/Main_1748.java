75edf521cd3c1f953e2cdf7c2e09aa0809202209
sukeun1997 <busan-sy@naver.com>
2021-10-07 01:14:13 +0900

    백준 10 / 7

---
+++ src/Main_1748.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main_1748 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0 , count = 1, length = 10;
+        for (int i = 1; i <= N ; i++) {
+
+            if(i == length) {
+                length *= 10;
+                count++;
+            }
+            sum += count;
+        }
+        bw.write(String.valueOf(sum));
+        bw.flush();
+    }
+}
