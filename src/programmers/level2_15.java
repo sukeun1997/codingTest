5b4c607db7bac1f822bdada56b704bbf07a238c9
sukeun1997 <busan-sy@naver.com>
2021-12-06 21:26:51 +0900

    프로그래머스 12/06

---
+++ src/programmers/level2_15.java
@@ -0,0 +1,40 @@
+package programmers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class level2_15 {
+
+    public int[] solution(int[] prices) {
+
+        int[] answer = new int[prices.length];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int pre = stack.pop();
+                answer[pre] = i - pre;
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            answer[current] = prices.length - current - 1;
+        }
+
+        return answer;
+    }
+
+
+    @Test
+    public void test() {
+        assertEquals(solution(new int[]{1, 2, 3, 2, 3}),new int[]{4, 3, 1, 1, 0});
+    }
+}
