f82346605ed76b1baf95bb177e315e88e0566261
sukeun <busan-sy@naver.com>
2022-01-16 21:59:18 +0900

    프로그래머스 1/16

---
+++ src/programmers/level2_21.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class level2_21 {
+
+
+    public int solution(int[] citations) {
+
+        Arrays.sort(citations);
+
+        int count = 0;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            count++;
+
+            if (citations[i] < count) {
+                return --count;
+            }
+
+        }
+
+        return count;
+    }
+
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(solution(new int[]{1, 3, 5, 7, 9, 11}), 4);
+        Assertions.assertEquals(solution(new int[]{1, 1, 5, 7, 6}), 3);
+        Assertions.assertEquals(solution(new int[]{9,9,9,12}), 4);
+    }
+}
