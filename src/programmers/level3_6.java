73358c5d9a13cd98806f5a1a963517060a6a9dc0
sukeun <busan-sy@naver.com>
2022-03-25 13:59:27 +0900

    프로그래머스 level3 단속 카메라

---
+++ src/programmers/level3_6.java
@@ -0,0 +1,56 @@
+package programmers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 단속 카메라
+ */
+public class level3_6 {
+
+    public int solution(int[][] routes) {
+
+        Arrays.sort(routes,(o1, o2) -> {
+            return o1[1] - o2[1];
+        });
+
+        int answer = 0;
+        int now = 0;
+
+        for (int i = 0; i < routes.length; i++) {
+            if (i == 0) {
+                now = routes[i][1];
+                answer++;
+                continue;
+            }
+
+            int start = routes[i][0];
+            int end = routes[i][1];
+
+            if (now >= start) {
+                continue;
+            }
+
+            now = end;
+            answer++;
+
+        }
+
+
+
+        return answer;
+    }
+
+
+    @Test
+    void test() {
+
+        assertEquals(solution(new int[][]{{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}}),2);
+        assertEquals(solution(new int[][]{{-20,-15},{-20,15}, {-14,-5}, {-18,-13}, {-5,-3}}),2);
+        assertEquals(solution(new int[][]{{-20,15},{-14,-5}, {-18,-13}, {-5,-3}}),2);
+    }
+}
