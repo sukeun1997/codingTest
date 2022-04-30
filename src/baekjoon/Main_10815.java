df5620cc43c04205d373bf4cb9b0e5e57745872f
sukeun <busan-sy@naver.com>
2022-04-30 12:32:54 +0900

    백준 10815

---
+++ src/baekjoon/Main_10815.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 숫자 카드
+ */
+public class Main_10815 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+//        boolean check[] = new boolean[]
+    }
+}
