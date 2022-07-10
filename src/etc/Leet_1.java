986fd770fbdf6f0055f2375104175f7a8a8cbc98
sukeun <busan-sy@naver.com>
2022-07-10 12:18:52 +0900

    Leet 1

---
+++ src/etc/Leet_1.java
@@ -0,0 +1,26 @@
+package etc;
+
+import java.util.HashMap;
+
+public class Leet_1 {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int result [] = new int[2];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(target - nums[i])) {
+                result[0] = i;
+                result[1] = map.get(target - nums[i]);
+                break;
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+}
