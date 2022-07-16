5e432d5bdcce413601e184c8cdbe30fffaa32469
sukeun <busan-sy@naver.com>
2022-07-16 19:01:43 +0900

    Leet 50 *

---
+++ src/etc/Leet_50.java
@@ -0,0 +1,30 @@
+package etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Spiral Matrix
+ */
+public class Leet_50 {
+
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> list = new ArrayList<>();
+        findOrder(0, 0, matrix, list);
+    }
+
+    private void findOrder(int x, int y, int[][] matrix, List<Integer> list) {
+
+        int yLen = matrix[0].length - 1;
+        int xLen = matrix.length - 1;
+        if (y != yLen) {
+            list.add(matrix[x][y]);
+            findOrder(x, y + 1, matrix, list);
+        } else if (x != xLen) {
+            list.add(matrix[x][y]);
+            findOrder(x + 1, y, matrix, list);
+        }
+    }
+}
