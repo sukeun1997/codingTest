50228390f089839117c5cbe8610a2db6df2a91b0
sukeun <busan-sy@naver.com>
2022-10-30 21:55:49 +0900

    백준 1158

---
+++ src/baekjoon/Main_1158_2.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main_1158_2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        Deque<Integer> circle = new LinkedList<>();
+        StringBuilder st = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            circle.add(i);
+        }
+
+        st.append("<");
+        while (!circle.isEmpty()) {
+            for (int i = 1; i < K; i++) {
+                circle.addLast(circle.removeFirst());
+            }
+            if (circle.size() > 1) {
+                st.append(circle.removeFirst()).append(", ");
+            } else {
+                st.append(circle.removeFirst());
+            }
+        }
+        st.append(">");
+        System.out.println(st);
+
+    }
+}
